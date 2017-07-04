d0b9213893ba25a448cac1888122098af19cfd2e
fbl <m15978760154_1@163.com>
2017-07-04 16:18:08 +0800

    'nothing'

--- mybatis/myBatis/src/test/java/com/kaishengit/Test.java
+++ mybatis/myBatis/src/test/java/com/kaishengit/Test.java
@@ -2,16 +2,29 @@ package com.kaishengit;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
 
 import com.kaishengit.entity.Student;
 import com.kaishengit.mapper.StudentMapper;
+import com.kaishengit.util.SqlSessionManager;
 
 public class Test {
+	SqlSession sqlSession;
+	StudentMapper studentMapper;
+	@Before
+	public void befer(){
+		 sqlSession=SqlSessionManager.getSqlSession(false);
+		 studentMapper=sqlSession.getMapper(StudentMapper.class);
+	}
+	
+	
 	
 	@org.junit.Test
 	public void find(){
@@ -22,7 +35,7 @@ public class Test {
 			SqlSessionFactory sessionFactory=sqlSessionFactoryBuilder.build(reader);
 			SqlSession sqlSession=sessionFactory.openSession();
 			
-			
+		
 			StudentMapper studentMapper=sqlSession.getMapper(StudentMapper.class);
 			
 //			Student stu =sqlSession.selectOne("com.kaishengit.mapper.StudentMapper.findById", 1);
@@ -41,4 +54,21 @@ public class Test {
 		
 	}
 	
+	
+	@org.junit.Test
+	public void findAll(){
+//		SqlSession sqlSession=SqlSessionManager.getSqlSession(false);
+//		StudentMapper studentMapper=sqlSession.getMapper(StudentMapper.class);
+		
+		List<Student> list=studentMapper.findAll();
+		for(Student stu:list){
+			System.out.println(stu.getName());
+		}
+		
+	}
+	
+	@After
+	public void after(){
+		sqlSession.close();
+	}
 }
