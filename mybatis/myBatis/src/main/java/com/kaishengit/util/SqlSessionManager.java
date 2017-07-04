f63108870c773acbe5e4014575a34996f0ba8369
fbl <m15978760154_1@163.com>
2017-07-04 16:09:27 +0800

    'tianjia'

--- mybatis/myBatis/src/main/java/com/kaishengit/util/SqlSessionManager.java
+++ mybatis/myBatis/src/main/java/com/kaishengit/util/SqlSessionManager.java
@@ -1,5 +1,31 @@
 package com.kaishengit.util;
 
-public class SqlSessionManager {
+import java.io.IOException;
+import java.io.Reader;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class SqlSessionManager {
+	public static SqlSessionFactory sessionFactory=getSqlSessionFactory();
+
+	private static SqlSessionFactory getSqlSessionFactory()  {
+			try {
+				Reader reader=Resources.getResourceAsReader("mybatis.xml");
+				SqlSessionFactory sessionFactory= new SqlSessionFactoryBuilder().build(reader);
+				return sessionFactory;
+				
+			} catch (IOException e) {
+				throw new RuntimeException("º”‘ÿ ß∞‹");
+			}
+	
+	}
+	public static SqlSession getSqlSession(boolean bl){
+		return sessionFactory.openSession(bl);
+		
+	}
+	
+	
 }
