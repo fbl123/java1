6d5e773417a2e3971a09e24f6cf735c3af5ccf6a
fbl <m15978760154_1@163.com>
2017-07-07 11:22:35 +0800

    'zhushi'

--- spring/src/test/java/com/kaishengit/dap/StuDaoTest.java
+++ spring/src/test/java/com/kaishengit/dap/StuDaoTest.java
@@ -4,7 +4,6 @@ import com.kaishengit.dao.StuDao;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 /**
  * Created by Administrator on 2017/7/7.
  */
@@ -12,6 +11,9 @@ public class StuDaoTest {
 
     @Test
     public void say(){
+        /**
+         *
+         */
         ApplicationContext applicationContext=new ClassPathXmlApplicationContext("applicationContext.xml");
         StuDao stuDao=(StuDao)applicationContext.getBean("sd");
         stuDao.say();
