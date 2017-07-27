fec25d50f5276edb37e45abbab8e9567c797b2fc
fbl <m15978760154_1@163.com>
2017-07-27 11:32:20 +0800

    固定通知

---
+++ crm/crm_service_impl/src/main/java/com/kaishengit/quartz/jobs/TellJob.java
@@ -0,0 +1,12 @@
+package com.kaishengit.quartz.jobs;
+
+import org.springframework.stereotype.Component;
+
+
+public class TellJob {
+
+
+    public void hello(){
+        System.out.println("hello word");
+    }
+}
